e5c9af917301697d29f48eddfc596f2a63963e31
Dainius <dainius.kreivenas@gmail.com>
2019-12-13 19:55:38 +0000

    Ensure run.sh works

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-mkdir -p parser/tests/results
+mkdir -p tests/results
 
-python parser/tests/Parse_ISawTheDog.py > ./parser/tests/results/parse_I_saw_the_dog.sp
+python tests/Parse_ISawTheDog.py > ./tests/results/parse_I_saw_the_dog.sp
